ab30a5e829ef9be3dc16745f1f9fc9328705411c
Aditya Kadlag <adityakadlag2@gmail.com>
2025-07-20 11:52:58 +0530

    Create datatype.java

---
+++ datatype.java
@@ -0,0 +1,17 @@
+
+   
+import java.util.*;
+
+public class datatype {
+
+	public static void main(String[] args) {
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter one number:");
+		int a=sc.nextInt();
+		System.out.println("value of a is"+a);
+
+	}
+
+}
+
+
