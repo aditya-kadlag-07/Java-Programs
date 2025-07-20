5bf892d8a53ecd564b3319b538201d33a204b16d
Aditya Kadlag <adityakadlag2@gmail.com>
2025-07-20 11:49:08 +0530

    Create CM1.java

---
+++ CM1.java
@@ -0,0 +1,49 @@
+
+
+public class CM1 {
+	/*static int b;
+	private int a;
+	public void demo() {
+	System.out.println("demo");	
+	
+	}
+	{
+	System.out.println("ns-block");
+	}
+	static {
+		System.out.println("Static Block");
+	}
+
+	public static void main(String[] args) {
+		System.out.println(b);
+		System.out.println(CM1.b);
+		CM1 obj1= new CM1();
+		System.out.println(obj1.b);
+		System.out.println(obj1.a);
+		CM1 obj2=new CM1();
+		obj2.a=20;
+		System.out.println(obj1.a);
+		obj1.demo();
+		
+
+	}
+
+}*/
+static int a=10;
+{
+	System.out.println(a);
+	System.out.println(CM1.a);
+	
+}
+public static void main(String[]args)
+{
+	CM1 a=new CM1();
+	
+	
+}
+public static void demo() {
+	// TODO Auto-generated method stub
+	
+}
+
+}
